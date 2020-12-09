8c9ba36327adfb31ee5137b596f5bd71e3304564
andrey <6097033725>
2020-12-09 22:58:59 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+FileReader Helper
+===================
+Classes for simple reading huge files
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+composer require andrewdanilov/php-filereader "~1.0.0"
+```
+
+or add
+
+```
+"andrewdanilov/php-filereader": "~1.0.0"
+```
+
+to the `require` section of your `composer.json` file.
+
+
+Usage
+-----
+
+todo
---
+++ src/FileReader.php
@@ -0,0 +1,69 @@
+<?php
+namespace classes;
+
+class FileReader
+{
+	/**
+	 * Обертка для функции fopen().
+	 * Открывает файл с учетом кодировок utf-16, utf-8.
+	 * Возвращает указатель на файл.
+	 * Результат чтения файла будет автоматически
+	 * сконвертирован в utf-8.
+	 *
+	 * @param string $filename
+	 * @param string $mode
+	 * @return false|resource
+	 */
+	private static function fopen($filename, $mode='r')
+	{
+		$handle = fopen($filename, $mode);
+		$bom = fread($handle, 2);
+		rewind($handle);
+
+		if ($bom === chr(0xff) . chr(0xfe) || $bom === chr(0xfe) . chr(0xff)) {
+			stream_filter_append($handle, 'convert.iconv.UTF-16LE/UTF-8');
+		}
+
+		return ($handle);
+	}
+
+	/**
+	 * Поиск последней строки в файле, с исключением строк
+	 * которые начинаются на заданные символы
+	 *
+	 * @param string $filePath
+	 * @param string $exceptLineFirstChar
+	 * @return false|string|null
+	 */
+	public static function getLastLine($filePath, $exceptLineFirstChar='\n')
+	{
+		$lastWord = null;
+		$f = static::fopen_utf($filePath, 'r');
+		if ($f) {
+			// ищем с конца к началу
+			if (fseek($f, -1, SEEK_END) == 0) { // в конец файла -1 символ перевода строки
+				// позиция последнего символа
+				$length = ftell($f);
+				// последний прочитанный символ
+				$last_char = '';
+				$pos = $length;
+				while ($pos > 0) {
+					fseek($f, -1, SEEK_CUR);
+					$char = fread($f, 1);
+					// если встретился признак конца строки и предыдущий
+					// символ не попадает в $exceptLineFirstChar
+					if ($char == "\n" && !preg_match('~^[' . $exceptLineFirstChar . ']$~', $last_char)) {
+						break;
+					}
+					$last_char = $char;
+					fseek($f, -1, SEEK_CUR);
+					$pos--;
+				}
+				// извлекаем строку с текущей позиции до первого переноса строки
+				$lastWord = fgets($f, $length - $pos + 1);
+			}
+			fclose($f);
+		}
+		return trim($lastWord);
+	}
+}
